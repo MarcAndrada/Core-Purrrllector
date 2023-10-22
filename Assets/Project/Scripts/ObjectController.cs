65e1f7968d8562970cb16e5eadf7fb269f8fce1b
Crami <crami.andrada@gmail.com>
2023-10-22 21:25:41 +0200

    Iba a hacer el inventario, al final no ;-;

---
+++ Assets/Project/Scripts/ObjectController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    public enum ObjectType { COOPER, EMERALD, ENEMY1, ENEMY2, PLANT };
+
+
+
+
+}
