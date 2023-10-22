0de4961b6d35922db7240198168b177d253a45db
Crami <crami.andrada@gmail.com>
2023-10-22 20:05:20 +0200

    Minigame System Done

--- Assets/Project/Scripts/MineMinigameManager.cs
+++ Assets/Project/Scripts/MineMinigameManager.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class MineMinigameManager : MonoBehaviour
 {
-    [SerializeField]
+    [Space, SerializeField]
     private float laserChargeSpeed;
     [SerializeField]
     private float laserDischargeSpeed;
@@ -24,9 +24,24 @@ public class MineMinigameManager : MonoBehaviour
     private MinigameBarController rightLaser;
     private bool chargingRightLaser;
 
-
+    [Space, Header("Mine"), SerializeField]
+    private Slider c_progressBarSlider;
+    [SerializeField]
+    private Slider c_integrity;
+    [SerializeField]
+    private float progressSpeed;
+    [SerializeField]
+    private float breakSpeed;
+    [SerializeField]
+    private float maxIntegrity;
+    private float progressValue;
+    private float integrityValue;
+    
     void Start()
     {
+        integrityValue = maxIntegrity;
+        c_progressBarSlider.maxValue = maxIntegrity;
+
         leftLaser.SetCurrentEnergyLevel(50f);
         rightLaser.SetCurrentEnergyLevel(50f);
 
@@ -39,11 +54,13 @@ public class MineMinigameManager : MonoBehaviour
         ManageInputs();
         SetLasersValue();
         CheckLasersEnergy();
-
+        CheckAdvanceProgress();
+        CheckProgressEnded();
 
         if (Input.GetKeyDown(KeyCode.M))
         {
             //Acabar de minar
+            EndMining();
         }
     }
 
@@ -93,11 +110,13 @@ public class MineMinigameManager : MonoBehaviour
         {
             //Tiene la energia necesaria
             _currentLaser.SetCurrentEnergyPointerColor(correctEnergyColor);
+            _currentLaser.CorrectEnergy = true;
         }
         else
         {
             //No tiene la energia necesaria
             _currentLaser.SetCurrentEnergyPointerColor(wrongEnergyColor);
+            _currentLaser.CorrectEnergy = false;
         }
     }
 
@@ -107,5 +126,75 @@ public class MineMinigameManager : MonoBehaviour
         rightLaser.SetNeedEnergyLevel(Random.Range(10f, 90f), Random.Range(neededSizes.x, neededSizes.y));
     }
 
+    private void CheckAdvanceProgress()
+    {
+        if (rightLaser.CorrectEnergy && leftLaser.CorrectEnergy)
+        {
+            //++
+            progressValue += (progressSpeed * 2) * Time.deltaTime;
+        }
+        else if(rightLaser.CorrectEnergy || leftLaser.CorrectEnergy)
+        {
+            //+-
+            progressValue += progressSpeed * Time.deltaTime;
+            integrityValue -= breakSpeed * Time.deltaTime;
+        }
+        else
+        {
+            //--
+            integrityValue -= (breakSpeed * 2) * Time.deltaTime;
+        }
+
+        c_progressBarSlider.value = progressValue;
+        c_integrity.value = integrityValue;
+    }
+
+
+
+    private void CheckProgressEnded()
+    {
+        if (progressValue >= 100 || integrityValue <= 0)
+        {
+            //Acabar de minar
+            EndMining();
+        }
+    }
+
+    private void EndMining()
+    {
+        GetMinerals();
+
+        progressValue = 0;
+        integrityValue = 0;
+
+        gameObject.SetActive(false);
+    }
+
+    private void GetMinerals()
+    {
+        float quarterIntegrity = maxIntegrity / 4;
+
+        if (integrityValue >= quarterIntegrity * 3) //100% de minerales
+        {
+            Debug.Log("100%");
+        }
+        else if (integrityValue >= quarterIntegrity * 2) //75% de minerales
+        {
+            Debug.Log("75%");
+        }
+        else if (integrityValue >= quarterIntegrity) //50% de minerales
+        {
+            Debug.Log("50%");
+        }
+        else if (integrityValue > 0) //25% de minerales
+        {
+            Debug.Log("25%");
+
+        }
+        else // 0% de minerales
+        {
+            Debug.Log("0%");
+        }
+    }
 
 }
