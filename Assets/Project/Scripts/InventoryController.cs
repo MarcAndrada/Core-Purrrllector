97cb0bbb669cce02a9d87e32b3c7e6d2a2d8271e
Cromo <crami.andrada@gmail.com>
2023-10-23 17:10:15 +0200

    Sistema de inventario, falta que se muestre en la UI

--- Assets/Project/Scripts/InventoryController.cs
+++ Assets/Project/Scripts/InventoryController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class InventoryController : MonoBehaviour
 {
-
-
-
     
+    private Dictionary<ObjectController.ObjectType, short> items;
 
-
-    public void AddObject()
+    private void Awake()
     {
-
+        items = new Dictionary<ObjectController.ObjectType, short>();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            AddObject(ObjectController.ObjectType.COOPER, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            AddObject(ObjectController.ObjectType.COOPER, -1);
+        }
+    }
+
+    public void AddObject(ObjectController.ObjectType _objectType, short _itemsToAdd)
+    {
+        if (!items.ContainsKey(_objectType))
+        {
+            items.Add(_objectType, _itemsToAdd);
+        }
+        else
+        {
+            items[_objectType] += _itemsToAdd;
+            if (items[_objectType] < 0)
+                items[_objectType] = 0;
+        }
+
+
+
+        Debug.Log(items[_objectType]);
+    }
+    
+    public short GetObject(ObjectController.ObjectType _objectType)
+    {
+        return items[_objectType];
+    }
 }
--- Assets/Project/Scripts/ObjectController.cs
+++ Assets/Project/Scripts/ObjectController.cs
@@ -7,6 +7,8 @@ public class ObjectController : MonoBehaviour
     public enum ObjectType { COOPER, EMERALD, ENEMY1, ENEMY2, PLANT };
 
 
+    [SerializeField]
+    public ObjectType type { get; private set;}
 
 
 }
